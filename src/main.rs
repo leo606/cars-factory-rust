f27a1a8a5609a10b16a3dca9f21965782e38e8bf
Leo Ferreira <leonardoferreira@protonmail.com>
2022-10-17 21:31:27 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![deny(clippy::all)]
+
+use std::env;
+
+const API_URL: &str = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
+
+fn main() {
+    println!("Hello, world!");
+}
