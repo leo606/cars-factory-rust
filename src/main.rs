19daba28b3cd513a888379e5dd0646bfda0b7ce2
Leo Ferreira <leonardoferreira@protonmail.com>
2022-10-17 21:41:53 -0300

    Manufacturer impl

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,24 @@ use std::env;
 
 const API_URL: &str = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json";
 
+struct Manufacturer<'a> {
+    name: Option<&'a str>,
+    common_name: Option<&'a str>,
+    country: Option<&'a str>,
+}
+
+trait Contains {
+    fn constains(&self, needle: &str) -> bool;
+}
+
+impl Contains for Manufacturer {
+    fn constains(&self, needle: &str) -> bool {
+        self.name.unwrap_or_default().contains(needle)
+            || self.common_name.unwrap_or_default().contains(needle)
+            || self.country.unwrap_or_default().contains(needle)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
